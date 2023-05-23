e3ecccf763e152ea9d09bc5d1508d191844ce1c9
Ubuntu <inko51366@naver.com>
2023-05-23 10:47:23 +0000

    폼이 하나라도 비어있을 경우 alert

--- find/find-id/find-id.js
+++ find/find-id/find-id.js
@@ -1,5 +1,14 @@
 function validate() {
+  const idValue = document.getElementById("id-text-field").value;
+  const nameValue = document.getElementById("name-text-field").value;
+  const phoneNumberValue = document.getElementById("phonenumber-text-field").value;
 
+  if (idValue === "" || nameValue === "" || phoneNumberValue === "") {
+    alert("입력되지 않은 폼이 있습니다");
+    return false;
+  }
+
+  return true;
 }
 
 const homebtn = document.getElementById("home-button");

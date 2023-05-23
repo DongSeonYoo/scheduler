7671f62219e6103aecf8f30493cbc563b9fdc91f
Ubuntu <ubuntu@ip-172-31-32-237.ap-northeast-2.compute.internal>
2023-05-23 08:57:50 +0000

    아이디나 비밀번호가 입력되었는지 검증

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function validate() {
-  
+  const idValue = document.getElementById("id-text-field").value;
+  const pwValue = document.getElementById("pw-text-field").value;
+
+  if (idValue === "" || pwValue === "") {
+    alert("아이디 또는 비밀번호를 입력해주세요");
+    return false;
+  }
+
+  return true;
 }

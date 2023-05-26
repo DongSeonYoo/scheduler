505d478f632bbec71bc568ca3e26006dc9970416
Ubuntu <inko51366@naver.com>
2023-05-26 05:04:52 +0000

    연도 이전, 이후 버튼 구현

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,1 +1,24 @@
 // 스케쥴러 js
+
+let yearSelectLabel;
+let currentYear;
+
+initYearSelectLabel();
+
+function initYearSelectLabel() {
+  yearSelectLabel = document.getElementById("year-select-label");
+  currentYear = "2023";
+  yearSelectLabel.innerHTML = currentYear + "년";
+}
+
+const yearPreviousBtn = document.getElementById("year-previous-button");
+yearPreviousBtn.addEventListener("click", () => {
+  currentYear = parseInt(currentYear) - 1;
+  yearSelectLabel.innerHTML = currentYear + "년";
+});
+
+const yearAfterBtn = document.getElementById("year-after-button");
+yearAfterBtn.addEventListener("click", () => {
+  currentYear = parseInt(currentYear) + 1;
+  yearSelectLabel.innerHTML = currentYear + "년";
+});
